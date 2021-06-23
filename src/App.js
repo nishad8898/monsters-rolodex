eb0cc7733faf9de2dbda45303ca257ada3e4060a
Sachin Nishad <nishadsachin8898@gmail.com>
2021-06-23 21:46:54 +0530

    First react project from the course

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React, {Component} from 'react';
+import {SearchBox} from './components/search-box/search-box.component.jsx';
+import {CardList} from './components/card-list/card-list.component.jsx';
+import './App.css';
+
+class App extends Component{
+  constructor(){
+    super();
+    this.state = {
+      monsters:[],
+      searchField:[]
+    }
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({monsters: users}))
+  }
+
+  handleChange = (e) => {
+    this.setState({searchField:e.target.value})
+  }
+
+  render(){
+
+    const {monsters, searchField} = this.state;
+    const filteredMonsters = monsters.filter(monster => 
+      monster.name.toLowerCase().includes(searchField.toString().toLowerCase())
+    );
+
+    return(
+      <div className='App'>
+        <h1>monsters Rolodex</h1>
+        <SearchBox
+          placeHolder = 'Search Monsters' 
+          handleChange = {this.handleChange}
+          />
+        <CardList monsters={filteredMonsters} />
+      </div>
+    )
+  }
+}
+
+export default App;
